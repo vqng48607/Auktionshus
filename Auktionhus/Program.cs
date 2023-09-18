ebec4ce1235693fb757d1d7b39a210b2257df1bd
Vuong <vuonquocnguyen2604@gmail.com>
2023-09-18 10:25:15 +0200

    Initial

---
+++ Auktionhus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Auktionhus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
